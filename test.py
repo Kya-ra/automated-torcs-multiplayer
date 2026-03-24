819ef111fa432cb44bf9b922ab23f17526991ed0
Kyara <kyaramcw@tcd.ie>
2026-03-24 13:54:20 +0000

    Pont test to faststart for testing

--- test.py
+++ test.py
@@ -41,4 +41,4 @@ for i in range(player_count):
     drivers_section.append(sec)
 
 tree.write(xml_path, encoding="UTF-8", xml_declaration=True)
-call(["./launch.sh", str(player_count)])
+call(["./faststart.sh", str(player_count)])
